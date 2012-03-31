326e802e1756ca34c97901df2f64fe865dc61a40
Jake Brukhman <jbrukh@gmail.com>
2012-03-31 01:10:08 -0400

    Fixed slash.

--- deskception.sh
+++ deskception.sh
@@ -3,7 +3,7 @@
 TMP_DIR="/tmp/deskception"
 NEW_DIR=$(date)
 
-mkdir -p "$TMP_DIR" && 
+mkdir -p "$TMP_DIR" && \
 mv ~/Desktop/* "$TMP_DIR"
 mkdir -p ~/Desktop/"$NEW_DIR" && \
 mv "$TMP_DIR"/* ~/Desktop/"$NEW_DIR" && \
