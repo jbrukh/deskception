31ae1d22340afcb8e053d69c117a855ba7ea0a88
Jake Brukhman <jbrukh@gmail.com>
2012-03-30 22:33:48 -0400

    First commit.

---
+++ deskception/deskception.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TMP_DIR="/tmp/deskception"
+NEW_DIR=$(date)
+
+mkdir -p "$TMP_DIR" && 
+mv ~/Desktop/* "$TMP_DIR"
+mkdir -p ~/Desktop/"$NEW_DIR" && \
+mv "$TMP_DIR"/* ~/Desktop/"$NEW_DIR" && \
+rm -rf "$TMP_DIR"
