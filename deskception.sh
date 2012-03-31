e3203ce4827d672bb6919243df4889fc369637fd
Jake Brukhman <jbrukh@gmail.com>
2012-03-31 02:08:01 -0400

    Added error handling.

--- deskception.sh
+++ deskception.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -o errexit
 
 TMP_DIR="/tmp/deskception"
 NEW_DIR=$(date)
+DESKTOP=~/Desktop
+
+# if the desktop is empty, do nothing
+if [ -z "$(ls $DESKTOP)" ]; then
+    exit
+fi
 
 mkdir -p "$TMP_DIR" && \
-mv ~/Desktop/* "$TMP_DIR"
-mkdir -p ~/Desktop/"$NEW_DIR" && \
-mv "$TMP_DIR"/* ~/Desktop/"$NEW_DIR" && \
+mv $DESKTOP/* "$TMP_DIR"
+mkdir -p $DESKTOP/"$NEW_DIR" && \
+mv "$TMP_DIR"/* $DESKTOP/"$NEW_DIR" && \
 rm -rf "$TMP_DIR"
